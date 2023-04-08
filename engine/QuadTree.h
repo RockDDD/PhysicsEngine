6614f652c9d7f709490d3abfac33a1cc8e9673b3
RockDDD <465166328@qq.com>
2023-04-08 17:11:44 +0800

    add quadTree

---
+++ engine/QuadTree.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "Geometry2D.h"
+
+struct QuadTreeData
+{
+	void* object;
+	Rectangle2D bounds;
+	bool flag;
+
+	inline QuadTreeData(void* o, const Rectangle2D& b) :
+		object(o), bounds(b), flag(false) { }
+};
+
+class QuadTreeNode
+{
+protected:
+	std::vector<QuadTreeNode> children;
+	vector<QuadTreeData*> contents;
+	int currentDepth;
+	static int maxDepth;
+	static int maxObjectsPerNode;
+	Rectangle2D nodeBounds;
+
+public:
+	inline QuadTreeNode(const Rectangle2D& bounds) :
+		nodeBounds(bounds), currentDepth(0) { }
+
+	bool IsLeaf();
+
+	int NumObjects();
+
+	void Insert(QuadTreeData& data);
+	void Remove(QuadTreeData& data);
+	void Update(QuadTreeData& data);
+
+	void Shake();
+	void Split();
+	void Reset();
+	std::vector<QuadTreeData*> Query(const Rectangle2D& area);
+};
+
+typedef QuadTreeNode QuadTree;
